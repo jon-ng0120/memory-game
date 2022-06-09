fa596bcf281c166ac7a736cc98040f85da36371a
Jon Ng <jonathanng15@gmail.com>
2022-06-08 19:44:32 -0700

    Added media queries

--- src/Components/Scoreboard.js
+++ src/Components/Scoreboard.js
@@ -3,18 +3,13 @@ import styled from 'styled-components';
 
 const Div = styled.div`
   display: flex;
-  // justify-content: center;
-  // box-shadow: rgba(6, 24, 44, 0.4) 0px 0px 0px 2px,
-  //   rgba(6, 24, 44, 0.65) 0px 4px 6px -1px,
-  //   rgba(255, 255, 255, 0.08) 0px 1px 0px inset;
   gap: 2rem;
-  // // margin: 2rem auto;
-  // // width: 20rem;
-  // background: #ebebeb;
-  // padding: 1rem;
-  // line-height: 1.5;
   font-size: 1.2rem;
   font-weight: 800;
+
+  @media (max-width: 768px) {
+    margin-top: 2rem;
+  }
 
   & div {
     text-align: center;
