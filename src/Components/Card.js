ed0cb7cf326d40db577f221d95c8ddaf4b15b8aa
Jon Ng <jonathanng15@gmail.com>
2022-06-10 00:03:33 -0700

    Changed card background to black

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,8 @@ import styled from 'styled-components';
 const Div = styled.div`
   box-shadow: rgba(0, 0, 0, 0.6) 0px 3px 8px;
   border-radius: 5px;
-  background: white;
+  background: rgba(0, 0, 0, 0.7);
+  color: rgba(255, 255, 255, 0.9);
   display: grid;
   grid-template-rows: 70% auto;
   height: 14rem;
@@ -14,6 +15,12 @@ const Div = styled.div`
   transition: all 0.15s ease-in-out;
   overflow: hidden;
   font-weight: 600;
+
+  & div:nth-child(2) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 
   &:hover {
     transform: scale(1.1);
